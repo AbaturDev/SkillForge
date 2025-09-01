bebc89efc1d2acf413dd973efd833093272acfa7
Abatur <alek@zolnowski.eu>
2025-09-01 20:53:12 +0200

    Add confirmation dialogs

--- frontend/src/pages/Skill.Details.tsx
+++ frontend/src/pages/Skill.Details.tsx
@@ -17,11 +17,16 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { SkillsService } from "../services/skills";
 import { toaster } from "../components/ui/toaster";
 import { useNavigate, useParams } from "react-router-dom";
+import { ConfirmDialog } from "../components/ConfirmDialog";
+import { useState } from "react";
 
 export const SkillDetails = () => {
   let { id } = useParams();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+  const [isEndOpen, setIsEndOpen] = useState(false);
 
   const deleteSkillMutation = useMutation<void, Error, string>({
     mutationFn: (id: string) => SkillsService.deleteSkill(id),
@@ -72,56 +77,80 @@ export const SkillDetails = () => {
   const formatDate = (date?: string) =>
     date ? new Date(date).toUTCString() : "-";
 
+  const onDeleteConfirm = () => {
+    deleteSkillMutation.mutate(data.id);
+    setIsDeleteOpen(false);
+  };
+
+  const onEndConfirm = () => {
+    endSkillMutation.mutate(data.id);
+    setIsEndOpen(false);
+  };
+
   return (
-    <Flex gap="4" justify="center">
-      <VStack>
-        <Card.Root size="lg" w="full" maxW="lg">
-          <Card.Header justifyContent="center" textAlign="center">
-            <Flex w="full" align="center" justify="space-between">
-              <Text fontSize="xl" fontWeight="bold" flex="1" textAlign="center">
-                {data?.name}
+    <>
+      <Flex gap="4" justify="center">
+        <VStack>
+          <Card.Root size="lg" w="full" maxW="lg">
+            <Card.Header justifyContent="center" textAlign="center">
+              <Flex w="full" align="center" justify="space-between">
+                <Text
+                  fontSize="xl"
+                  fontWeight="bold"
+                  flex="1"
+                  textAlign="center"
+                >
+                  {data?.name}
+                </Text>
+              </Flex>
+            </Card.Header>
+
+            <Card.Body>
+              <Text color="fg.muted" mb={4}>
+                {data.description}
               </Text>
-            </Flex>
-          </Card.Header>
+              <Stack gap={2}>
+                <Text>Started at: {formatDate(data.createdAt)}</Text>
+                {data.endedAt && (
+                  <Text>Finished at: {formatDate(data.endedAt)}</Text>
+                )}
+              </Stack>
+            </Card.Body>
 
-          <Card.Body>
-            <Text color="fg.muted" mb={4}>
-              {data.description}
-            </Text>
-            <Stack gap={2}>
-              <Text>Started at: {formatDate(data.createdAt)}</Text>
-              {data.endedAt && (
-                <Text>Finished at: {formatDate(data.endedAt)}</Text>
-              )}
-            </Stack>
-          </Card.Body>
-
-          <Card.Footer justifyContent="space-between">
-            <Badge colorPalette={mapBadgeColor[data.skillStatus]}>
-              {mapSkillStatus(data.skillStatus)}
-            </Badge>
-            <Text fontSize="sm" color="gray.500" fontStyle="italic">
-              Last update: {formatDate(data.updatedAt)}
-            </Text>
-          </Card.Footer>
-        </Card.Root>
-        <HStack justifyContent={"space-between"} w="full">
-          <Button
-            colorPalette={"red"}
-            onClick={() => deleteSkillMutation.mutate(data.id)}
-          >
-            <MdDeleteForever /> Delete
-          </Button>
-          <Button
-            colorPalette={"blue"}
-            disabled={data.skillStatus === 1}
-            onClick={() => endSkillMutation.mutate(data.id)}
-          >
-            <FaCalendarTimes />
-            End
-          </Button>
-        </HStack>
-      </VStack>
-    </Flex>
+            <Card.Footer justifyContent="space-between">
+              <Badge colorPalette={mapBadgeColor[data.skillStatus]}>
+                {mapSkillStatus(data.skillStatus)}
+              </Badge>
+              <Text fontSize="sm" color="gray.500" fontStyle="italic">
+                Last update: {formatDate(data.updatedAt)}
+              </Text>
+            </Card.Footer>
+          </Card.Root>
+          <HStack justifyContent={"space-between"} w="full">
+            <Button colorPalette={"red"} onClick={() => setIsDeleteOpen(true)}>
+              <MdDeleteForever /> Delete
+            </Button>
+            <Button
+              colorPalette={"blue"}
+              disabled={data.skillStatus === 1}
+              onClick={() => setIsEndOpen(true)}
+            >
+              <FaCalendarTimes />
+              End
+            </Button>
+          </HStack>
+        </VStack>
+      </Flex>
+      <ConfirmDialog
+        isOpen={isDeleteOpen}
+        onClose={() => setIsDeleteOpen(false)}
+        onConfirm={onDeleteConfirm}
+      />
+      <ConfirmDialog
+        isOpen={isEndOpen}
+        onClose={() => setIsEndOpen(false)}
+        onConfirm={onEndConfirm}
+      />
+    </>
   );
 };
